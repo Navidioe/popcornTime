00575576139f6b9705436d1c2132729c9c8b79f3
Balram Khichar <beingbalram@gmail.com>
2016-08-20 16:13:09 +0530

    Form component added

---
+++ src/components/BookingFormComponent.js
@@ -0,0 +1,47 @@
+'use strict';
+require('styles/BookingForm.scss');
+
+import React from 'react';
+
+class BookingFormComponent extends React.Component {
+	constructor(props) {
+    super(props);
+		this.state = {
+			name: '',
+			seats: ''
+		}
+		this.handleNameChange = this.handleNameChange.bind(this);
+		this.handleSeatsChange = this.handleSeatsChange.bind(this);
+		this.handleSubmitForm = this.handleSubmitForm.bind(this);
+  }
+
+	handleNameChange(event) {
+		this.setState({name: event.target.value});
+	}
+
+	handleSeatsChange(event) {
+		this.setState({seats: event.target.value});
+	}
+
+	handleSubmitForm(event) {
+		event.preventDefault();
+		this.props.handleBookingForm(this.state.name, this.state.seats);
+	}
+
+  render() {
+    return (
+      <form className="booking-form" onSubmit={this.handleSubmitForm}>
+        <div className="input-wrapper">
+        	<input type="text" value={this.state.name} onChange={this.handleNameChange} placeholder="Name" required/>
+        </div>
+        <div className="input-wrapper">
+        	<input type="number" value={this.state.seats} onChange={this.handleSeatsChange} placeholder="No. of seats" min="1" max="10" required/>
+        </div>
+        <div className="submit-wrapper">
+        	<button type="submit">Start Selecting</button>
+        </div>
+      </form>
+    );
+  }
+}
+export default BookingFormComponent;
